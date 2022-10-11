cf7cf892e2dec8b4a390a7f9a6b18efae4f0a869
KY-U <uscaio@estudante.ufscar.br>
2022-10-11 17:48:46 -0300

    trigger

--- Delivery Driver/Assets/Collision.cs
+++ Delivery Driver/Assets/Collision.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class Collision : MonoBehaviour
 {
+    //sempre que colide com um objeto
     void OnCollisionEnter2D(Collision2D other) {
         Debug.Log("puxa vida!");
+    }
+
+    //sempre que ativa um trigger
+    void OnTriggerEnter2D(Collider2D other) {
+       Debug.Log("eita glória!"); 
     }
 }
